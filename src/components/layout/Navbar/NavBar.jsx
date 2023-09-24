45f41d8a5c35732e06ad3cac0e013490a8f22f9a
MalenaJuarezM <malenajuarezm@gmail.com>
2023-09-24 18:44:56 +0200

    Primera entraga sin comentarios

--- src/components/layout/Navbar/NavBar.jsx
+++ src/components/layout/Navbar/NavBar.jsx
@@ -2,12 +2,10 @@
 import { Typography } from "@mui/material";
 import { CartWidget } from "../../common/CartWidget/CartWidget"
 import "./Navbar.css"
-// import Button from '@mui/material/Button';
 
 export const Navbar = () => {
     return (
         <div className="navbar">
-            {/* <Button variant="outlined" color="primary">Contained</Button> */}
             <img src="https://res.cloudinary.com/dqy0anqjn/image/upload/v1695570628/Logo1_uhakbd.png" alt="Logo" className="logo" />
             <Typography align="center" variant="h5" color={"primary.main"}>
                 <ul className="menu">
@@ -20,6 +18,5 @@ export const Navbar = () => {
             </Typography>
             <CartWidget />
         </div>
-
     )
 }
